bd70ba1f29df3e520b3884810de1e11c8ca66d68
Chris Campo <ccampo133@users.noreply.github.com>
2020-03-24 18:26:59 -0400

    Fix time formatting and watermark

--- covid19visuals/constants.py
+++ covid19visuals/constants.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
 BASE_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data'
 TIME_SERIES_BASE_URL = f'{BASE_URL}/csse_covid_19_time_series'
@@ -6,5 +6,5 @@ CONFIRMED_GLOBAL_TIME_SERIES = f'{TIME_SERIES_BASE_URL}/time_series_covid19_conf
 CONFIRMED_REGIONAL_TIME_SERIES = f'{TIME_SERIES_BASE_URL}/time_series_19-covid-Confirmed.csv'
 GLOBAL_DATE_FORMAT = '%m/%d/%Y'
 REGIONAL_DATE_FORMAT = '%m/%d/%y'
-NOW = datetime.utcnow()
+NOW = datetime.utcnow().replace(tzinfo=timezone.utc)
 TODAY = NOW.date()
--- covid19visuals/templating.py
+++ covid19visuals/templating.py
@@ -5,7 +5,7 @@ import importlib.resources as pkg_resources
 
 def build_index_html():
     html = pkg_resources.read_text(templates, 'index.html')
-    last_updated = f'{constants.NOW.replace(microsecond=0).isoformat()}Z'
+    last_updated = f'{constants.NOW.replace(microsecond=0).strftime("%A, %d %B %Y, %H:%M %Z")}'
     rendered = pystache.render(html, {'last_updated': last_updated})
     with open('index.html', 'w') as f:
         f.write(rendered)
