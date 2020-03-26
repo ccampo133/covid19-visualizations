30623b168f8c62df6f17edc04ab9cb6a7c749d65
Chris Campo <ccampo133@users.noreply.github.com>
2020-03-25 20:45:13 -0400

    Add reference exponential growth (#4)
    * Add reference exponential growth

--- covid19visuals/analysis.py
+++ covid19visuals/analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from datetime import datetime
 from covid19visuals import constants
 
@@ -12,33 +13,20 @@ def get_latest_total(data: pd.DataFrame, country: str = None):
     return filtered[filtered.columns.values[-1]].sum()
 
 
-def get_days_deaths(data: pd.DataFrame, starting_deaths: int):
-    days, deaths = [], []
+def get_days_cases(data: pd.DataFrame, start: int):
+    days, cases = [], []
 
     # Parse only the date columns
     t0 = None
     for col in data.columns.values[4:]:
-        cur_deaths = data[col].sum()
-        if cur_deaths >= starting_deaths:
+        cur_cases = data[col].sum()
+        if cur_cases >= start:
             date = datetime.strptime(col, constants.REGIONAL_DATE_FORMAT).date()
             if t0 is None:
                 t0 = date
             delta = (date - t0).days
             days.append(delta)
-            deaths.append(data[col].sum())
-
-    return days, deaths
-
-
-def get_days_cases(data: pd.DataFrame):
-    days, cases = [], []
-
-    # Parse only the date columns
-    for col in data.columns.values[4:]:
-        date = datetime.strptime(col, constants.REGIONAL_DATE_FORMAT).date()
-        delta = (date - constants.TODAY).days
-        days.append(delta)
-        cases.append(data[col].sum())
+            cases.append(data[col].sum())
 
     return days, cases
 
@@ -48,3 +36,7 @@ def get_death_rate(deaths: pd.DataFrame, cases: pd.DataFrame):
     total_cases = cases[latest_date].sum()
     total_deaths = deaths[latest_date].sum()
     return 100 * (total_deaths / total_cases)
+
+
+def exponential_growth(t, x0, r):
+    return x0 * np.power((1 + r), t)
